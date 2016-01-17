78b411800ab83f17cb16d2fa596defe1357a9c9a
Bartek Brak <bartek.rychlicki@gmail.com>
2016-01-17 18:34:15 +0100

    Add README.rst
    Instead of markdown to please PyPI.

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
   name='bowerbird',
   packages=['bowerbird'],
-  version='0.1',
+  version='0.1.1',
   description=(
       'A collection of stdlib logging.Formatter classes using Pygments'),
   install_requires=['Pygments'],
   author='Bartek Brak',
   author_email='bartek0brak@gmail.com',
   url='https://github.com/bartekbrak/bowerbird',
-  download_url='https://github.com/bartekbrak/bowerbird/archive/0.1.tar.gz',
+  download_url='https://github.com/bartekbrak/bowerbird/archive/0.1.1.tar.gz',
 )
