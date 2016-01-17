ee134ee6034daf361b524eb1909ef5c5af130666
Bartek Brak <bartek.rychlicki@gmail.com>
2016-01-17 20:02:03 +0100

    Bump to 0.2.1
    Don't require Pygments. They should be installed manually.

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 setup(
   name='bowerbird',
   packages=['bowerbird'],
-  version='0.2.0',
+  version='0.2.1',
   description=(
       'A collection of stdlib logging.Formatter classes using Pygments'),
   author='Bartek Brak',
   author_email='bartek0brak@gmail.com',
   url='https://github.com/bartekbrak/bowerbird',
-  download_url='https://github.com/bartekbrak/bowerbird/archive/0.2.0.tar.gz',
+  download_url='https://github.com/bartekbrak/bowerbird/archive/0.2.1.tar.gz',
 )
