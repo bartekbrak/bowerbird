29f361d348860f80fe62bd7a7a5a30d0ea0ce6c8
Bartek Brak <bartek.rychlicki@gmail.com>
2016-01-17 20:13:21 +0100

    Bump to 0.2.2

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 setup(
   name='bowerbird',
   packages=['bowerbird'],
-  version='0.2.1',
+  version='0.2.2',
   description=(
       'A collection of stdlib logging.Formatter classes using Pygments'),
   author='Bartek Brak',
   author_email='bartek0brak@gmail.com',
   url='https://github.com/bartekbrak/bowerbird',
-  download_url='https://github.com/bartekbrak/bowerbird/archive/0.2.1.tar.gz',
+  download_url='https://github.com/bartekbrak/bowerbird/archive/0.2.2.tar.gz',
 )
