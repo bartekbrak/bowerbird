1ded4d153cff154ac60e9921f94474c37a2c1fa8
Bartek Brak <bartek.rychlicki@gmail.com>
2016-02-26 21:57:24 +0100

    Bump to 0.4.0

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
   name='bowerbird',
   packages=['bowerbird'],
-  version='0.3.0',
+  version='0.4.0',
   description=(
       'A collection of stdlib logging.Formatter classes using Pygments'),
   author='Bartek Brak',
   install_requires=['sqlparse==0.1.18', 'Pygments'],
   author_email='bartek0brak@gmail.com',
   url='https://github.com/bartekbrak/bowerbird',
-  download_url='https://github.com/bartekbrak/bowerbird/archive/0.3.0.tar.gz',
+  download_url='https://github.com/bartekbrak/bowerbird/archive/0.4.0.tar.gz',
 )
